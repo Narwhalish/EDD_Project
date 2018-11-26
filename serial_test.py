756194f140bd891ef88aa5977c90203ab7288137
Narwhalish <bryao@ctemc.org>
2018-11-26 13:09:18 -0500

    Testing serial works

--- serial_test.py
+++ serial_test.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 # -*- encoding: utf-8 -*-
 
+from time import sleep
 import serial
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-s = [0]
+ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
+counter = 32 # Below 32 everything in ASCII is gibberish
 
 while True:
-    read_serial = ser.readline()
-    s[0] = str(int (ser.readline(), 16))
+     counter +=1
+     ser.write(str(chr(counter)).encode('utf-8')) # Convert the decimal number to ASCII then send it to the Arduino
 
-    print(s[0])
-    print(read_serial)
+     print(ser.readline()) # Read the newest output from the Arduino
+
+     sleep(.1) # Delay for one tenth of a second
+
+     if counter == 255:
+         counter = 32
