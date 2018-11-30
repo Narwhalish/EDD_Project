7874c35c6496e6ef25de53df3dfa0525639d3ae3
ivwang46 <ivy.wang121101@gmail.com>
2018-11-30 12:17:28 -0500

    EDD.py stuff

--- EDD.py
+++ EDD.py
@@ -1,5 +1,17 @@
 import tkinter as tk
+import serial
 
+#remember to install serial and RPi
+ser = serial.Serial("/dev/ttyACM0", 9600)
+#execute ls/dev/tty* when arduino is connected to raspi
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+root.title("EDD Interface")
+
+
+"""
 class Application(tk.Frame):
     def __init__(self, master):
         self.master = master
@@ -11,18 +23,24 @@ class Application(tk.Frame):
         button-new-volume = Button(master, text = "Input New Volume", command = volume)
         button-cont-pour = Button(master, text = "Continue Pour", command = pour)
         button-clean = Button(master, text = "Clean", command = clean)
+"""
+def volume():
+    pass
 
-        
-        
-    def volume():
-        pass
+def pour():
+    pass
 
-    def pour():
-        pass
+def clean():
+    pass
 
-    def clean():
-        pass
 
-def main():
-    app = Application()
-
+volume = tk.Button(frame,
+                   text="Input New Volume",
+                   command = volume)
+volume.pack(side=tk.LEFT)
+cont = tk.Button(frame,
+                   text="Continue Pour", command = pour)
+cont.pack(side=tk.LEFT)
+clean = tk.Button(frame, text="Clean", command = clean)
+clean.pack(side=tk.LEFT)
+root.mainloop()
