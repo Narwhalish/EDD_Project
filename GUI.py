6bd7326b15e492a4fffaadc7b6b6d6aea231ebe7
ivwang46 <ivy.wang121101@gmail.com>
2018-12-02 21:35:40 -0500

    class??

--- GUI.py
+++ GUI.py
@@ -1,6 +1,25 @@
 import tkinter as tk
 import main
 
+class GUI(tk.Tk):
+    volume = 0
+
+    def __init__(self):
+        frame = tk.Frame(self)
+        frame.pack()
+        self.title("Project Pour Interface")
+
+    def volume():
+        number = tk.Entry(frame, textvariable = var)
+        number.pack()
+        confirm = tk.Button(frame, text = "Ok", command = input)
+        confirm.pack()
+
+    def input():
+        volume = tk.number.get()
+        print(volume)
+
+"""
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
@@ -15,9 +34,9 @@ def volume():
     confirm.pack()
 
 def input():
-    volume = root.number.get()
+    volume = tk.number.get()
     print(volume)
-
+"""
 volume = tk.Button(frame, text="Input New Volume", command = volume)
 volume.pack(side=tk.LEFT)
 cont = tk.Button(frame, text="Continue Pour", command = main.pour)
